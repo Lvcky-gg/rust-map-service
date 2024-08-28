1e2a58db90cca5104b0ca36e52467181d92c6bde
lvcky-gg <matadorcrab@gmail.com>
2024-08-28 15:48:34 -0400

    angular?

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, afterRender } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Map from '@arcgis/core/Map';
+import SceneView from '@arcgis/core/views/SceneView';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend';
+  myMap = null;
+  sceneView = null;
+  afterRender() {
+    this.myMap = new Map({
+      basemap: 'streets',
+    });
+    this.sceneView = new SceneView({
+      map: this.myMap,
+      container: 'mapDiv',
+    });
+  }
 }
